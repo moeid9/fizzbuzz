06950a9c406c29ce32f195cb3149048d313b011a
marie taylor <marietaylortailor@hotmail.com>
2023-01-10 10:08:30 +0000

    Fizzbuzz-spec-1to3-added

--- spec/fizzbuss_spec.rb
+++ spec/fizzbuss_spec.rb
@@ -4,3 +4,9 @@ describe 'fizzbuzz' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
 end
+    it 'returns "1" when passed 1' do
+        expect(fizzbuzz(1)).to eq '1'
+    end
+    it 'returns "2" when passed 2' do
+        expect(fizzbuzz(2)).to eq '2'
+    end
